173e14874780eebb647decbf5cad6a99b99db52e
Adam Goth <adamgoth@gmail.com>
2016-05-30 09:30:01 -0600

    Adds dummy search bar, adds more states and region data to picker

--- src/components/campsitesMap.js
+++ src/components/campsitesMap.js
@@ -4,6 +4,7 @@ import {
   Picker,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View
 } from 'react-native';
@@ -14,8 +15,9 @@ import Regions from '../data/regions'
 module.exports = React.createClass({
   getInitialState: function() {
     return {
+      searchTerm: '',
       state: 'us',
-      region: {longitudeDelta: 131.8359405964179, latitude: 52.49195113130104, longitude: -104.7812499009146, latitudeDelta: 69.54145563738211}
+      region: {longitudeDelta: 131.8359405964179, latitude: 52.49195113130104, longitude: -104.7812499009146, latitudeDelta: 69.54145563738211},
     };
   },
 
@@ -48,15 +50,40 @@ module.exports = React.createClass({
           style={styles.map}>
         </MapView>
         <View style={styles.bottomView}>
+        <TextInput
+          style={styles.searchBar}
+          defaultValue="Search by campsite name..."
+          onChangeText={(text) => this.setState({searchTerm: text})}
+          value={this.state.searchTerm}
+        />
           <Picker
             style={styles.picker}
             selectedValue={this.state.state}
             onValueChange={this.onPickerChange}>
             <Picker.Item label="All US" value={'us'} />
+            <Picker.Item label="Alabama" value={'alabama'} />
+            <Picker.Item label="Alaska" value={'alaska'} />
+            <Picker.Item label="Arizona" value={'arizona'} />
+            <Picker.Item label="Arkansas" value={'arkansas'} />
+            <Picker.Item label="California" value={'california'} />
             <Picker.Item label="Colorado" value={'colorado'} />
+            <Picker.Item label="Connecticut" value={'connecticut'} />
+            <Picker.Item label="Delaware" value={'delaware'} />
             <Picker.Item label="Florida" value={'florida'} />
             <Picker.Item label="Illinois" value={'illinois'} />
+            <Picker.Item label="Indiana" value={'indiana'} />
+            <Picker.Item label="Iowa" value={'iowa'} />
             <Picker.Item label="Georgia" value={'georgia'} />
+            <Picker.Item label="Hawaii" value={'hawaii'} />
+            <Picker.Item label="Idaho" value={'idaho'} />
+            <Picker.Item label="Kansas" value={'kansas'} />
+            <Picker.Item label="Kentucky" value={'kentucky'} />
+            <Picker.Item label="Louisiana" value={'louisiana'} />
+            <Picker.Item label="Maine" value={'maine'} />
+            <Picker.Item label="Maryland" value={'maryland'} />
+            <Picker.Item label="Massachusetts" value={'massachusetts'} />
+            <Picker.Item label="New Mexico" value={'new mexico'} />
+            <Picker.Item label="Utah" value={'utah'} />
           </Picker>
         </View>
       </View>
@@ -85,12 +112,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'stretch',
   },
+  searchBar: {
+    padding: 10,
+    marginTop: 20,
+    height: 40,
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
   map: {
-    flex: 1,
+    flex: 4,
     marginTop: 30
   },
   bottomView: {
-    flex: 1,
+    flex: 3,
   },
   detailsButton: {
     justifyContent: 'center',
